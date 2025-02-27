3bdcd77f0140046d36f1efcc2f000a591830f8df
juangrings <64484463+juangrings@users.noreply.github.com>
2025-02-27 17:40:48 -0300

    Add files via upload
    Jogo do número. Meu primeiro projeto!

---
+++ app.js
@@ -0,0 +1,15 @@
+alert('Bem-vindo ao jogo do 100. Se você passar dos 100 pontos, você ganha! :)');
+pontuacao1 = prompt('Você é uma pessoa legal?');{
+    if(pontuacao1 == 'sim'){
+        alert('Parabéns! Você ganhou 15 pontos! Anote num papel')
+ } else alert('Credo')
+};
+pontuacao2 = prompt('Você torce para o Grêmio?');{
+    if(pontuacao2 == 'sim'){
+        alert('Dale Grêmio! Você ganhou 86 pontos! Anote num papel')
+    } else {'Feche a aba, colorado'}
+};
+resultado = prompt('Quantos pontos você fez?');{
+    if(resultado >= 100)
+        alert('Te amo <3')
+};
